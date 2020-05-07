499e7398641a1193f48f16958ab81b1bd659ac52
Kenneth Ossai <kenossai@ymail.com>
2020-05-07 23:07:13 +0100

    addded redux to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
+import { createStore, applyMiddleware } from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import Thunk from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import rootReducer from './rootReducer'
 
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(Thunk))
+)
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
